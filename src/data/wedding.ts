689816ca6a76a61d9a3a1f1eed547f2a08891657
Minh Quang <vixenogs@gmail.com>
2026-04-21 17:48:14 +0700

    new update hình save the date

--- src/data/wedding.ts
+++ src/data/wedding.ts
@@ -218,7 +218,7 @@
 
   export const images = {
     countdownBg:
-      'https://drive.google.com/file/d/1CKbjX_RCpRlIDkhfx2sjwBcyonzL6WmE?w=1400&h=900&fit=crop&q=80',
+      'https://drive.google.com/file/d/1CKbjX_RCpRlIDkhfx2sjwBcyonzL6WmE',
     coupleOverlapLeft:
       'https://images.unsplash.com/photo-1522673607200-164d1b6ce486?w=400&h=400&fit=crop&q=80',
     coupleOverlapRight:
